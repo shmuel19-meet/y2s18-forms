357794a24a41cb128ed95ce6683de85c11f67c1e
Shmuel19-meet <shmuel19-meet@meet.mit.edu>
2018-08-08 18:11:32 +0300

    works

--- exercises/app.py
+++ exercises/app.py
@@ -1,13 +1,21 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', students=query_all())
+	return render_template('home.html', students=query_all())
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
-    return render_template('student.html', student=query_by_id(student_id))
+	return render_template('student.html', student=query_by_id(student_id))
+
+@app.route('/add', methods = ['GET', 'POST'])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template("add.html")
+	print("This is a post request")
+	add_student(request.form['student_name'], request.form['student_year'], False)
+	return render_template("add.html")
 
 app.run(debug=True)
